f1348754ce09df1bce23e19822cdce8effc4725b
Joanna Chądzyńska <jmalek87@gmail.com>
2021-02-09 19:44:09 +0100

    update home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,36 @@
+import md5 from 'md5';
 import React from 'react';
 
 export interface HomePageProps {}
 
 const HomePage: React.SFC<HomePageProps> = () => {
+	interface MethodProps {
+		method: string;
+		track?: string;
+		artist?: string;
+		album?: string;
+	}
+	const getApiSignature = ({ method }: MethodProps) => {
+		let sessionKey = localStorage.getItem('key'),
+			apiKey = process.env.REACT_APP_LAST_FM_API_KEY,
+			secretKey = process.env.REACT_APP_LAST_FM_SHARED_SECRET;
+		const params = {
+			method,
+			api_key: apiKey,
+			sk: sessionKey,
+		};
+
+		let string = [];
+		for (let [key, value] of Object.entries(params)) {
+			const pair = `${key}${value}`;
+			string.push(pair);
+		}
+		string.toString().split(',').join('');
+		const apiSig = md5(`${string}${secretKey}`);
+		return apiSig;
+	};
+	getApiSignature({ method: 'track.love' });
+
 	return <div>home page</div>;
 };
 
